ab17149d1247551c38e182a5aae3501bcb77f90e
Hammad Umar <hammadhawc227@gmail.com>
2025-02-28 18:43:24 +0500

    create endpoint for getting profile of logged in user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,19 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { JwtPayload } from '../auth/types/jwt-payload.type';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 @ApiTags('User')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('/me')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  async me(@CurrentUser() { _id }: JwtPayload) {
+    return this.usersService.me(_id);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -21,4 +21,10 @@ export class UsersService {
   ): Promise<UserDocument> {
     return this.usersRepository.findOne(filterQuery);
   }
+
+  async me(_id: string): Promise<UserDocument> {
+    const user = await this.usersRepository.findOne({ _id });
+    delete user.password;
+    return user;
+  }
 }
